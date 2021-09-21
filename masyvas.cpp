78d868ab9f851d98e5d2165ec1a0d88b74135ea5
UgniusBraun <91115862+UgniusBraun@users.noreply.github.com>
2021-09-21 11:59:55 +0300

    Update and rename main.cpp to masyvas.cpp

---
+++ masyvas.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct studentas {
+ string Vardas[];
+ string Pavarde[];
+ int NamuDarbas[];
+ int Egzaminas[];
+};
+
+int main() {
+ int n;
+ studentas S[];
+ 
+ 
+}
