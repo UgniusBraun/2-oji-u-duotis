bf233e592e628a1962b8ec36680e6dd72c58dfc3
UgniusBraun <91115862+UgniusBraun@users.noreply.github.com>
2021-10-07 12:00:19 +0300

    Update vektorius.cpp
    Realizacija su vektoriais buvo atlikta vis dar naudojantis masyvais, ir su šiuo kodo atnaujinimu pataisiau šią klaidą.

--- vektorius.cpp
+++ vektorius.cpp
@@ -7,38 +7,39 @@ struct studentas
     vector <double> NamuDarbai;
     int Egzaminas, Suma=0;
     double Vidurkis, Mediana;
-    int N;
+    int N = 0;
 };
 
-void nuskaitymas(int &m, studentas masyvas[]);
-void skaiciavimas(int m, studentas masyvas[]);
-void isvedimas(int m, studentas masyvas[]);
+void nuskaitymas(vector<studentas> &vektorius);
+void skaiciavimas(vector<studentas> &vektorius);
+bool f (const studentas &a, const studentas &b);
+void isvedimas(vector<studentas> &vektorius);
 
 int main()
 {
-    int m=1;
-    studentas masyvas[100];
-    nuskaitymas(m, masyvas);
-    skaiciavimas(m, masyvas);
-    isvedimas(m, masyvas);
+    vector<studentas> vektorius;
+    nuskaitymas(vektorius);
+    skaiciavimas(vektorius);
+    isvedimas(vektorius);
 }
 
-void nuskaitymas(int &m, studentas masyvas[])
+void nuskaitymas(vector<studentas> &vektorius)
 {
     int tikrinimas;
-    int tikrinimas2;
-    int egzaminas;
-    char random;
-    int laikinas;
-        for (int i=0; i<100; i++)
+        int tikrinimas2;
+        int egzaminas;
+        char random;
+        int laikinas;
+        for (int i=0;;i++)
         {
+            studentas stud;
             cout << "Iveskite " << i+1 << " o-jo studento varda bei pavarde:" << endl;
-            cin >> masyvas[i].Vardas >> masyvas[i].Pavarde;
-            for (int j=0; j<100; j++)
+            cin >> stud.Vardas >> stud.Pavarde;
+            for (int j=0;;j++)
             {
                 cout << "Ar yra dar namu darbu pazymiu? Jei taip, spauskite 1, jei ne, spauskite 2" << endl;
                 cin >> tikrinimas2;
-                if ((tikrinimas2 == 2 && masyvas[i].N == 0) || tikrinimas2 == 1)
+                if ((tikrinimas2 == 2 && stud.N == 0) || tikrinimas2 == 1)
                 {
                     if (tikrinimas2 == 2)
                     {
@@ -49,8 +50,8 @@ void nuskaitymas(int &m, studentas masyvas[])
                     cin >> random;
                     if (random == 'r' || random == 'R')
                     {
-                        masyvas[i].NamuDarbai.push_back((rand() % 10) + 1);
-                        masyvas[i].N++;
+                        stud.NamuDarbai.push_back((rand() % 10) + 1);
+                        stud.N++;
                     }
                     else
                     {
@@ -65,8 +66,8 @@ void nuskaitymas(int &m, studentas masyvas[])
                         }
                         else
                         {
-                            masyvas[i].NamuDarbai.push_back(laikinas);
-                            masyvas[i].N++;
+                            stud.NamuDarbai.push_back(laikinas);
+                            stud.N++;
                         }
                     }
                 }
@@ -79,83 +80,91 @@ void nuskaitymas(int &m, studentas masyvas[])
             cout << "Ar norite " << i+1 << " -am studentui atsitiktinai sugeneruoti egzamino pazymi? Jei taip, spauskite 'r' klavisa, jei ne, bet koki kita klavisa" << endl;
             cin >> random;
             if (random == 'r' || random == 'R')
+            {
+                stud.Egzaminas = (rand() % 10) + 1;
+            }
+            else
+            {
+                cout << "Iveskite " << i+1 << " -ojo studento egzamino rezultata [1-10]:" << endl;
+                while (egzaminas < 1 || egzaminas > 10)
                 {
-                    masyvas[i].Egzaminas = (rand() % 10) + 1;
-                }
-                else
-                {
-                            cout << "Iveskite " << i+1 << " -ojo studento egzamino rezultata [1-10]:" << endl;
-                    while (egzaminas < 1 || egzaminas > 10)
+                    cin >> egzaminas;
+                    if (egzaminas >= 1 && egzaminas <= 10)
                     {
-                        cin >> egzaminas;
-                        if (egzaminas >= 1 && egzaminas <= 10)
-                        {
-                            masyvas[i].Egzaminas = egzaminas;
-                        }
-                        else
-                        {
-                            cin.clear();
-                            cout << "Ivedete netinkama pazymi! Bandykite vesti nauja pazymi [1-10]" << endl;
-                            cin.ignore(256, '\n');
-                        }
+                        stud.Egzaminas = egzaminas;
+                    }
+                    else
+                    {
+                        cin.clear();
+                        cout << "Ivedete netinkama pazymi! Bandykite vesti nauja pazymi [1-10]" << endl;
+                        cin.ignore(256, '\n');
                     }
                 }
+            }
             cout << "Ar yra dar studentu? Jei taip, spauskite 1, jei ne, spauskite 2." << endl;
             cin >> tikrinimas;
+            vektorius.push_back(stud);
             if (tikrinimas == 2)
             {
                 break;
             }
-            else m++;
         }
 }
 
-void skaiciavimas(int m, studentas masyvas[])
+
+void skaiciavimas(vector<studentas> &vektorius)
 {
-    for (int i=0; i<m; i++)
+    for (int i=0; i<vektorius.size(); i++)
     {
-        sort(masyvas[i].NamuDarbai.begin(), masyvas[i].NamuDarbai.end());
+        sort(vektorius[i].NamuDarbai.begin(), vektorius[i].NamuDarbai.end());
 
-        if (masyvas[i].N % 2 != 0)
+        if (vektorius[i].N % 2 != 0)
         {
-            masyvas[i].Mediana = (masyvas[i].NamuDarbai[(masyvas[i].N/2)]);
+            vektorius[i].Mediana = (vektorius[i].NamuDarbai[(vektorius[i].N/2)]);
         }
-        else masyvas[i].Mediana = (masyvas[i].NamuDarbai[(masyvas[i].N/2)-1] + masyvas[i].NamuDarbai[masyvas[i].N/2]) / 2;
+        else vektorius[i].Mediana = (vektorius[i].NamuDarbai[(vektorius[i].N/2)-1] + vektorius[i].NamuDarbai[vektorius[i].N/2]) / 2;
 
-        for (int j=0; j<masyvas[i].N; j++)
+        for (int j=0; j<vektorius[i].N; j++)
         {
-            masyvas[i].Suma += masyvas[i].NamuDarbai[j];
+            vektorius[i].Suma += vektorius[i].NamuDarbai[j];
 
         }
-        masyvas[i].Vidurkis = masyvas[i].Suma / masyvas[i].N;
+        vektorius[i].Vidurkis = vektorius[i].Suma / (double)vektorius[i].N;
     }
 }
 
-void isvedimas(int m, studentas masyvas[])
+bool f (const studentas &a, const studentas &b)
+{
+    return a.Vardas < b.Vardas;
+}
+
+void isvedimas(vector<studentas> &vektorius)
 {
     int tikrinimas;
     cout << "Ar norite, jog programa apskaiciuotu vidurki, ar mediana? Jei norite vidurkio, spauskite 1, jei medianos, spauskite 2." << endl;
     cin >> tikrinimas;
 
+    sort(vektorius.begin(), vektorius.end(), f);
     if (tikrinimas == 1)
+    {
+        cout << setw(15) << "Pavarde" << setw(15) << "Vardas" << setw(25) << setw(25) << "Galutinis (Vid.)" << endl;
+        cout << "-------------------------------------------------------" << endl;
+        for (int i=0; i<vektorius.size(); i++)
         {
-            cout << setw(15) << "Pavarde" << setw(15) << "Vardas" << setw(25) << setw(25) << "Galutinis (Vid.)" << endl;
-            cout << "-------------------------------------------------------" << endl;
-            for (int i=0; i<m; i++)
-            {
-                cout << setw(15) << masyvas[i].Vardas << setw(15) << masyvas[i].Pavarde;
-                cout << setw(25) << fixed << setprecision(2) << masyvas[i].Vidurkis * 0.4 + masyvas[i].Egzaminas * 0.6 << endl;
-            }
+            cout << setw(15) << vektorius[i].Vardas << setw(15) << vektorius[i].Pavarde;
+            cout << setw(25) << fixed << setprecision(2) << vektorius[i].Vidurkis * 0.4 + vektorius[i].Egzaminas * 0.6 << endl;
         }
+    }
     else
     {
         cout << setw(15) << "Pavarde" << setw(15) << "Vardas" << setw(25) << setw(25) << "Galutinis (Med.)" << endl;
         cout << "-------------------------------------------------------" << endl;
-        for (int i=0; i<m; i++)
+        for (int i=0; i<vektorius.size(); i++)
         {
-            cout << setw(15) << masyvas[i].Vardas << setw(15) << masyvas[i].Pavarde;
-            cout << setw(25) << fixed << setprecision(2) << masyvas[i].Mediana * 0.4 + masyvas[i].Egzaminas * 0.6 << endl;
+            cout << setw(15) << vektorius[i].Vardas << setw(15) << vektorius[i].Pavarde;
+            cout << setw(25) << fixed << setprecision(2) << vektorius[i].Mediana * 0.4 + vektorius[i].Egzaminas * 0.6 << endl;
         }
     }
 
 }
+
